df233ac4cc76e6292b737086c31135422e645880
Viktor Shakhovtsov <94041640+shakhovtsov@users.noreply.github.com>
2022-09-07 00:04:48 +0300

    Add files via upload

---
+++ 03.seafood/seafood/js/main.js
@@ -0,0 +1,28 @@
+$(function(){
+    $('.header-slider').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt=""></button>',
+        vertical: true
+        // fade: true //чтобы картинка менялась без прокрутки
+    });
+
+
+    $('.product__name').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        focusOnSelect: true,
+        asNavFor: '.product__content',
+        vertical: true,
+        prevArrow: '<button type="button" class="product-prev"><img src="images/product-prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="product-next"><img src="images/product-next.svg" alt=""></button>',
+      });
+      $('.product__content').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.product__name',
+        fade: true,
+        arrows: false
+      });
+                      
+});
